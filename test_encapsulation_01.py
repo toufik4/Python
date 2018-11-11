76bb3e3667236524961ef63fa1052e8e9f9e8262
toufik <toufik4@yahoo.fr>
2018-11-11 20:12:15 +0100

    Encapsulation Simple methode type _myMethod() not accesible directly

---
+++ test_encapsulation_01.py
@@ -0,0 +1,18 @@
+# Remarks :
+# Python Version before 2.2 : print should be print()
+#!/usr/bin/env python
+ 
+class Car:
+ 
+    def __init__(self):
+        self.__updateSoftware()
+ 
+    def drive(self):
+        print 'driving'
+ 
+    def __updateSoftware(self):
+        print 'updating software'
+ 
+redcar = Car()
+redcar.drive()
+#redcar.__updateSoftware()  not accesible from object.
